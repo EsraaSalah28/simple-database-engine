664bfedf6fd2847493b64373f595c42f390bcfd6
EsraaSalah28 <es83626@gmail.com>
2020-01-25 23:39:37 +0200

    implement deleteDB

---
+++ deletedb.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+function delete ()
+{
+    isDeleted=false;
+    while [ ! -d $deletedDb ]
+    do
+echo Enter the name of DB you want to delete
+read deletedDb
+if [ -d $deletedDb ]
+then
+isDeleted=true;
+rm -r $deletedDb
+break;
+else
+echo  There is no such a database rewrite it again
+
+fi
+done
+}
+echo Enter the name of DB you want to delete
+read deletedDb
+if [ -d $deletedDb ]
+then
+rm -r $deletedDb
+else
+echo  There is no such a database rewrite it again
+delete
+fi
